3da1f50ce08bebdea7bae2548c353dd5dd26ef17
yushar <yusharw@gmail.com>
2024-08-15 12:52:11 +0700

    fix: :bug: add some validation on sendMessage endpoint

--- src/modules/message/service/message.service.ts
+++ src/modules/message/service/message.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Message } from "../model/message.schema";
@@ -16,13 +16,22 @@ export class MessageService {
     receiverId: string,
     content: string
   ): Promise<Message> {
-    const message = new this.messageModel({
+    if (!receiverId) {
+      throw new BadRequestException("Receiver ID is undefined");
+    }
+    if (receiverId === senderId) {
+      throw new BadRequestException("Cannot send message to yourself");
+    }
+    if (!content || content.trim() === "") {
+      throw new BadRequestException("Message content cannot be empty");
+    }
+
+    return new this.messageModel({
       senderId,
       receiverId,
       content,
-      timestamp: new Date(),
-    });
-    return message.save();
+      timestamp: Date.now(),
+    }).save();
   }
 
   async getMessages(userId: string, receiverId: string): Promise<Message[]> {
