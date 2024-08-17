c6232ab9ebc39d30aa801e11ac69f55c63f8a2a5
yushar <yusharw@gmail.com>
2024-08-17 08:42:57 +0700

    fix: :bug: add CORS setting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { CommonResponseInterceptor } from './common/interceptors/common-response.interceptor';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { CommonResponseInterceptor } from "./common/interceptors/common-response.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new CommonResponseInterceptor());
   await app.listen(3000);
